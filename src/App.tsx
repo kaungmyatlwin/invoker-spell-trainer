46cc572f0b131179cac85765348e113e537c1ef1
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2023-09-01 01:30:03 +0700

    spell shuffle logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import './App.scss'
 import _ from 'lodash';
 import { useEffect, useMemo, useState } from 'react'
 import useSpellQueue from './hooks/useSpellQueue'
-import { defaultKeyBindings, initialSpells, spells, ultSpell } from './types/constants'
+import { defaultKeyBindings, INITIAL_SPELLS, spells, ultSpell } from './types/constants'
 import { InvokeSpell } from './types/spells.types'
 import SpellButton from './components/SpellButton.react';
 
 function App() {
   const { spellQueue, queue } = useSpellQueue()
-  const [spell, setSpell] = useState<InvokeSpell | undefined>()
+  const [invokedSpells, setInvokedSpells] = useState<InvokeSpell[]>([])
 
   const ultDisabled = useMemo(() => spellQueue.length < 3, [spellQueue])
 
@@ -45,36 +46,55 @@ function App() {
 
   const onClickOrbCast = () => {}
   const onClickUlt = () => {
-    if (!spells[spellQueue]) {
-      // Perform fuzzy search
-      const currentSpellMap = getSpellMapping(spellQueue)
+    // Perform fuzzy search
+    const currentSpellMap = getSpellMapping(spellQueue)
+    const foundSpellMap = allSpellMappings
+      .find((spellMap) => _.isEqual(currentSpellMap, spellMap))
 
-      const foundSpellMap = allSpellMappings
-        .find((spellMap) => _.isEqual(currentSpellMap, spellMap))
+    const spellKey = Object.entries(foundSpellMap ?? {})
+      .reduce((acc, [randomSpellKey, spellOccurrence]) => {
+        let loopIterations = 0
 
-      const spellKey = Object.entries(foundSpellMap ?? {})
-        .reduce((acc, [randomSpellKey, spellOccurrence]) => {
-          let loopIterations = 0
+        while (spellOccurrence > loopIterations) {
+          acc = acc.concat(randomSpellKey)
 
-          while (spellOccurrence > loopIterations) {
-            acc = acc.concat(randomSpellKey)
+          loopIterations += 1
+        }
+        return acc
+      }, '')
 
-            loopIterations += 1
-          }
-          return acc
-        }, '')
+    // Check if we are about to have same spells
+    // TODO: refactor this logic into a hook?
+    const hasSameSpell = invokedSpells.find((s) => s.name === spells[spellKey].name)
+    if (hasSameSpell && invokedSpells.length === 1) {
+      return
+    }
+    if (hasSameSpell && invokedSpells.length === 2) {
+      if (invokedSpells[0] === spells[spellKey]) {
+        return
+      }
 
-        return setSpell(spells[spellKey])
+      return setInvokedSpells([
+        invokedSpells[1],
+        invokedSpells[0],
+      ])
     }
 
-    return setSpell(spells[spellQueue])
+    const invokeSpellsToSet = [spells[spellKey], ...invokedSpells]
+    // Get rid of last spell if 2 slots are full
+    if (invokedSpells.length === 2) {
+      invokeSpellsToSet.pop()
+    }
+
+    return setInvokedSpells(invokeSpellsToSet)
   }
 
   return (
     <div className="App">
+      <div className="App__playScene"></div>
       <div className="App__SpellButtonWrapper">
         {
-          initialSpells.map((spell) => <SpellButton key={spell.name} {...spell} />)
+          [...INITIAL_SPELLS, ...invokedSpells].map((spell) => spell && <SpellButton key={spell.name} {...spell} />)
         }
         <SpellButton {...ultSpell} disabled={ultDisabled} />
       </div>
